08ad1c1c82b81bcfe1d10665d8b9d58978b2362d
Tony Schroeder <tony.schroeder@gmx.de>
2022-05-15 19:41:03 +0200

    add stats about

--- js/script.js
+++ js/script.js
@@ -56,39 +56,62 @@ async function morePokemon() {
 
 function showBiger(i) {
     document.getElementById('blurContainer').classList.remove('d-none');
-
     let firstType = allPokemons[i]['types'][0]['type']['name'];
     let secoundType = '';
     if (allPokemons[i]['types'][1]) {
         secoundType = allPokemons[i]['types'][1]['type']['name']
     }
-    document.getElementById('bigCart').innerHTML = /*html*/ `
-            <div id="pokeCard" class="color-white ${firstType}-bg">
-                <img  onclick="closeBiger()" class="filter-withe closeicon" src="icon/close.png" alt="">
-                <div id="closeContainer" class="d-flex j-space-betwen">
-                    <img id="leftswipe" onclick="swipeLeft(${i})" class="filter-withe" src="icon/left.png" alt="">
-                    <img onclick="swipeRight(${i})" class="filter-withe" src="icon/right.png" alt="">
-                </div>
-                <h3 id="pokeId">#${i}</h3>
-                <img id="pokeImg" src="${allPokemons[i]['sprites']['other']['home']['front_default']}" alt="">
-                <div id="pokeName" class="text-uppercase"><h2>${allPokemons[i]['name']}</h2>
-                    <div id="type2" class="d-flex">
-                    <span class="${firstType}" >${firstType}</span>
-                    <span class="${secoundType}">${secoundType}</span>
-                    </div>
-                    </div>
-                <div id="infoCard">
-                    <button id="statusButton" onclick="openStats(${i})">Stats</button>
-                    <table id="statsTable" class=""></table>
-                </div>
-            </div>
-`;
+    document.getElementById('bigCart').innerHTML = showBigerText(i, firstType, secoundType);
     if (i == 1) {
         document.getElementById('leftswipe').classList.add('d-none');
         document.getElementById('closeContainer').classList.remove('space-between');
         document.getElementById('closeContainer').classList.add('j-flex-end');
-
     }
+    if (!secoundType) {
+        document.getElementById('secoundtype').classList.remove('mar-pad-wid');
+    }
+    setTimeout(removeBounceAnimation, 1200);
+}
+
+
+function showBigerText(i, firstType, secoundType) {
+    return /*html*/ `
+    <div id="pokeCard" class="color-white animation-bounceInUp ${firstType}-bg">
+        <img  onclick="closeBiger()" class="filter-withe closeicon" src="icon/close.png" alt="">
+        <div id="closeContainer" class="d-flex j-space-betwen">
+            <img id="leftswipe" onclick="swipeLeft(${i})" class="filter-withe" src="icon/left.png" alt="">
+            <img onclick="swipeRight(${i})" class="filter-withe" src="icon/right.png" alt="">
+        </div>
+        <h3 id="pokeId">#${i}</h3>
+        <img id="pokeImg" src="${allPokemons[i]['sprites']['other']['home']['front_default']}" alt="">
+        <div id="pokeName" class="text-uppercase"><h2>${allPokemons[i]['name']}</h2>
+            <div id="type2" class="d-flex j-center ">
+            <span class="${firstType} mar-pad-wid" >${firstType}</span>
+            <span id="secoundtype" class="${secoundType} mar-pad-wid">${secoundType}</span>
+            </div>
+            </div>
+        <div id="infoCard">
+        <img  onclick="closeData(${i})" id="closedata" class="filter-withe closeicon d-none" src="icon/down.png" alt="">
+
+            <div class="buttonContainer d-flex">
+                <div class="d-flex f-colum a-center">
+                    <button id="aboutButton" onclick="openAbout(${i})">About</button>
+                    <div id="aboutShadow" class="buttonshadow d-none"></div>
+                </div>
+                <div class="d-flex f-colum a-center">
+                    <button id="statusButton" onclick="openStats(${i})">Stats</button>
+                    <div id="statsShadow" class="buttonshadow d-none"></div>
+                </div>
+                <div class="d-flex f-colum a-center">
+                    <button id="movesButton" onclick="openMoves(${i})">Moves</button>
+                    <div id="movesShadow" class="buttonshadow d-none"></div>
+                </div>
+            </div>
+            <table id="statsTable" class=""></table>
+        </div>
+    </div>
+`;
+
 }
 
 
@@ -96,37 +119,119 @@ function closeBiger() {
     document.getElementById('blurContainer').classList.add('d-none');
 }
 
+function removeBounceAnimation() {
+    document.getElementById('pokeCard').classList.remove('animation-bounceInUp');
+
+}
 
 function swipeLeft(i) {
     i = i - 1;
+
     showBiger(i);
+    removeBounceAnimation();
 }
 
-function swipeRight(i) {
+
+
+async function swipeRight(i) {
     i = i + 1;
+    if (i >= allPokemons.length) {
+        beginPokemon = displayPokemon + 1;
+        displayPokemon = displayPokemon + 1;
+        maxPokemon = displayPokemon + 1;
+        await allPokemon();
+    }
     showBiger(i);
+    removeBounceAnimation();
 }
 
 
-// function renderPokemonInfo() {
-//     let containerId = document.getElementById('pokeId');
-//     let containerImg = document.getElementById('pokeImg');
-//     let containerName = document.getElementById('pokeName');
-//     containerId.innerHTML = ` < h3 > #$ { currentPokemon['id'] } < /h3>`;
-//     containerImg.src = currentPokemon['sprites']['other']['home']['front_default'];
-//     containerName.innerHTML = `<h2>${currentPokemon['name']}</h2>`;
-// }
+function openAbout(i) {
+    document.getElementById('pokeImg').classList.remove('animation-scaleup');
+    document.getElementById('pokeImg').classList.add('animation-scaledown');
+    document.getElementById('infoCard').classList.remove('animation-fadedown');
+    document.getElementById('infoCard').classList.add('animation-fadeup');
+    document.getElementById('pokeName').classList.add('d-none');
+    document.getElementById('closedata').classList.remove('d-none');
+    document.getElementById('aboutShadow').classList.remove('d-none');
+    document.getElementById('statsShadow').classList.add('d-none');
+    document.getElementById('movesShadow').classList.add('d-none');
+    document.getElementById('pokeId').innerHTML = `<h2>${allPokemons[i]['name']}</h2>`;
+    aboutText(i);
 
+}
 
 function openStats(i) {
-    document.getElementById('pokeImg').classList.toggle('animation-scaledown');
-    document.getElementById('infoCard').classList.toggle('animation-fadeup');
-    document.getElementById('pokeName').classList.toggle('d-none');
+    document.getElementById('pokeImg').classList.remove('animation-scaleup');
+    document.getElementById('pokeImg').classList.add('animation-scaledown');
+    document.getElementById('infoCard').classList.remove('animation-fadedown');
+    document.getElementById('infoCard').classList.add('animation-fadeup');
+    document.getElementById('pokeName').classList.add('d-none');
+    document.getElementById('closedata').classList.remove('d-none');
+    document.getElementById('statsShadow').classList.remove('d-none');
+    document.getElementById('aboutShadow').classList.add('d-none');
+    document.getElementById('movesShadow').classList.add('d-none');
     document.getElementById('pokeId').innerHTML = `<h2>${allPokemons[i]['name']}</h2>`;
     statsText(i);
-
 }
 
+
+function openMoves(i) {
+    document.getElementById('pokeImg').classList.remove('animation-scaleup');
+    document.getElementById('pokeImg').classList.add('animation-scaledown');
+    document.getElementById('infoCard').classList.remove('animation-fadedown');
+    document.getElementById('infoCard').classList.add('animation-fadeup');
+    document.getElementById('pokeName').classList.add('d-none');
+    document.getElementById('closedata').classList.remove('d-none');
+    document.getElementById('movesShadow').classList.remove('d-none');
+    document.getElementById('aboutShadow').classList.add('d-none');
+    document.getElementById('statsShadow').classList.add('d-none');
+    document.getElementById('pokeId').innerHTML = `<h2>${allPokemons[i]['name']}</h2>`;
+    // movesText(i);
+}
+
+
+function closeData(i) {
+    document.getElementById('statsTable').innerHTML = '';
+    document.getElementById('pokeImg').classList.remove('animation-scaledown');
+    document.getElementById('pokeImg').classList.add('animation-scaleup');
+    document.getElementById('infoCard').classList.remove('animation-fadeup');
+    document.getElementById('infoCard').classList.add('animation-fadedown');
+    document.getElementById('pokeName').classList.remove('d-none');
+    document.getElementById('closedata').classList.add('d-none');
+    document.getElementById('aboutShadow').classList.add('d-none');
+    document.getElementById('statsShadow').classList.add('d-none');
+    document.getElementById('movesShadow').classList.add('d-none');
+    document.getElementById('pokeId').innerHTML = `<h2>${allPokemons[i]['id']}</h2>`;
+}
+
+function aboutText(i) {
+    document.getElementById('statsTable').innerHTML = '';
+
+    document.getElementById('statsTable').innerHTML += ` 
+            <tr>
+                <th>Species</th>
+                <th>${allPokemons[i]['species']['name']}</th>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <th>#${i}</th>
+            </tr> 
+            <tr>
+                <th>Height</th>
+                <th>#${allPokemons[i]['height']}</th>
+            </tr> 
+            <tr>
+                <th>Weight</th>
+                <th>#${allPokemons[i]['weight']}</th>
+            </tr> 
+            <tr>
+                <th>Abilities</th>
+                <th>#${i}</th>
+            </tr> 
+            `;
+
+}
 
 function statsText(i) {
     document.getElementById('statsTable').innerHTML = '';
@@ -143,8 +248,17 @@ function statsText(i) {
     }
 }
 
-
-// function removeanimation() {
-//     document.getElementById('pokeImg').classList.remove('animation-scaledown');
-//     document.getElementById('infoCard').classList.remove('animation-fadeup');
-// }
+function movesText(i) {
+    document.getElementById('statsTable').innerHTML = '';
+    for (let j = 0; j < allPokemons[i]['stats'].length; j++) {
+        const element = allPokemons[i]['stats'][j];
+        document.getElementById('statsTable').innerHTML += ` 
+            <tr>
+                <th>${element['stat']['name'].replace(/special-/i, "Sp. ")}</th>
+                <th>${element['base_stat']}
+                    <span><div style="width: ${element['base_stat']}%;"></div></span>
+                </th>
+            </tr>   
+            `;
+    }
+}
